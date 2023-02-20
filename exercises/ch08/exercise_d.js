26fe8dc794a32c035f46127e8877d21162e02b09
Andrey Nikiforov <andy.nikiforov@gmail.com>
2023-02-19 17:37:08 -0800

    ch08

--- exercises/ch08/exercise_d.js
+++ exercises/ch08/exercise_d.js
@@ -13,7 +13,10 @@
 // Remember either's two arguments must return the same type.
 
 // validateName :: User -> Either String ()
-const validateName = undefined;
+const validateName = user => (user.name.length > 3 ? Either.of(null) : left('too short name'));
 
 // register :: User -> IO String
-const register = compose(undefined, validateUser(validateName));
+const register = compose(
+    either(IO.of, compose(map(showWelcome), save)),
+    validateUser(validateName),
+);
