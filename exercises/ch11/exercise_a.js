45ff746a8f5d907344ccfdf71d4d7df2f79d24cb
Andrey Nikiforov <andy.nikiforov@gmail.com>
2023-02-21 18:50:51 -0800

    ch11

--- exercises/ch11/exercise_a.js
+++ exercises/ch11/exercise_a.js
@@ -1,4 +1,8 @@
 // Write a natural transformation that converts `Either b a` to `Maybe a`
 
 // eitherToMaybe :: Either b a -> Maybe a
-const eitherToMaybe = undefined;
+// const eitherToMaybe = e => e.isRight ? Maybe.of(e.$value) : Maybe.of(null);
+
+// const eitherToMaybe = either(() => Maybe.of(null), Maybe.of);
+
+const eitherToMaybe = either(always(nothing), Maybe.of);
