49d13a5fd4e58b2adb4f27f7dfafa477fd726e0d
Andrey Nikiforov <andy.nikiforov@gmail.com>
2023-02-25 21:43:25 -0800

    ch12

--- exercises/ch12/exercise_c.js
+++ exercises/ch12/exercise_c.js
@@ -5,5 +5,18 @@
 //
 // Use traversable to rearrange and flatten the nested Tasks & Maybe
 
+// const { readfile } = require("../support");
+
 // readFirst :: String -> Task Error (Maybe (Task Error String))
-const readFirst = compose(map(map(readfile('utf-8'))), map(safeHead), readdir);
+// const readFirst = compose(map(map(readfile('utf-8'))), map(safeHead), readdir);
+
+// readFirst :: String -> Task Error (Maybe String)
+const readFirst = compose(
+    chain(
+        // Task Error (Task Error (Maybe String))
+        traverse(Task.of, readfile('utf-8')),
+    ),
+    // Task Error (Maybe String)
+    map(safeHead),
+    readdir,
+);
